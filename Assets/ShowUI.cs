dc7f9bf9b3d69c8f27798c9722e65af09decee66
JephCTaylor <jctaylor2nd@gmail.com>
2019-05-10 14:30:12 -0400

    Initial commit

---
+++ Assets/ShowUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AVC.Unity.UI;
+
+public class ShowUI : MonoBehaviour
+{
+    private static PopupMenu menu = new PopupMenu();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
