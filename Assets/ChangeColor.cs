9512e1c60abf2e70930a171bfb4e3bc4f6c8c385
JephCTaylor <jctaylor2nd@gmail.com>
2019-05-10 23:10:24 -0400

    added color dropdown for the control cube

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
+    Color colorEnd = Color.red;
+    Renderer rend;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rend.material.color = colorEnd;
+    }
+
+    public void ColorSelection(int color)
+    {
+        switch (color)
+        {
+            case 0:
+                colorEnd = Color.red;
+                break;
+            case 1:
+                colorEnd = Color.green;
+                break;
+            case 2:
+                colorEnd = Color.blue;
+                break;
+        }
     }
 }
