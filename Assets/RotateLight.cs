27e2027a384f4a519b2d0623025ddb57a6e40e95
JephCTaylor <jctaylor2nd@gmail.com>
2019-05-10 23:37:44 -0400

    added slider to change sun rotation speed

---
+++ Assets/RotateLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+    public float speed = 0;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+
+    public void AdjustSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+}
