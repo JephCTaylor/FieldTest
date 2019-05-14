1c5a580880804082961496f845921f4bce6abf7b
JephCTaylor <jctaylor2nd@gmail.com>
2019-05-14 05:12:32 -0400

    added cube toggle

---
+++ Assets/ToggleCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCube : MonoBehaviour
+{
+    public Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+    
+    public void Toggle()
+    {
+        if (rend.enabled)
+        {
+            rend.enabled = false;
+        }
+        else
+        {
+            rend.enabled = true;
+        }
+    }
+}
